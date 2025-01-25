8bf122ae3007c17903b31a9629c00935c6916871
jack2yin <jackjack.yin@gmail.com>
2025-01-25 14:25:50 -0500

    Completed exercise / Learned about recursive method to factorial a number

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,30 @@
-const add = function() {
-	
+const add = function(...numbers) {
+ return numbers.reduce((sum, current) => sum + current, 0)
 };
 
-const subtract = function() {
-	
+const subtract = function(...numbers) {
+	return numbers.reduce((sum, current) => (-sum) + (-current), 0)
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((sum, current) => sum + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+	return numbers.reduce((sum, current) => sum * current)
 };
 
-const power = function() {
-	
+const power = function(...numbers) {
+	return numbers.reduce((num,pw) => Math.pow(num, pw))
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(number) {
+  if (number == 0 || number == 1) {
+    return 1;
+  }
+
+  return number * factorial(number - 1)
+}
 
 // Do not edit below this line
 module.exports = {
